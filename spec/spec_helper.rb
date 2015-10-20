cbccb24c297227aac23340b2ea455c33dfac8888
sue445 <sue445@sue445.net>
2015-10-20 22:09:21 +0900

    Allow report send to codeclimate.com
    https://travis-ci.org/sue445/faraday_boolean/jobs/86393592

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -115,4 +115,8 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  config.before :each do
+    WebMock.disable_net_connect!(allow: "codeclimate.com")
+  end
 end
