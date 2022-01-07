5e1c31fe2dcd08d82601dd850c7f3e121943368a
sue445 <sue445@sue445.net>
2022-01-07 22:20:20 +0900

    Migrate to faraday v2

--- lib/faraday_boolean.rb
+++ lib/faraday_boolean.rb
@@ -1,10 +1,2 @@
 require "faraday_boolean/version"
-require "faraday"
-
-module FaradayBoolean
-  autoload :ParseBoolean, "faraday_boolean/parse_boolean"
-
-  Faraday::Response.register_middleware(
-    boolean: -> { ParseBoolean },
-  )
-end
+require "faraday_boolean/parse_boolean"
--- lib/faraday_boolean/parse_boolean.rb
+++ lib/faraday_boolean/parse_boolean.rb
@@ -1,17 +1,17 @@
+require "faraday"
+
 module FaradayBoolean
   # When response body is "true" or "false" (String), convert to true or false (Ruby literal)
-  class ParseBoolean < ::Faraday::Response::Middleware
-    def call(env)
-      response = @app.call(env)
-
-      case response.env[:body]
+  class ParseBoolean < ::Faraday::Middleware
+    def on_complete(env)
+      case env[:body]
       when "true"
-        response.env[:body] = true
+        env[:body] = true
       when "false"
-        response.env[:body] = false
+        env[:body] = false
       end
-
-      response
     end
   end
 end
+
+Faraday::Response.register_middleware(boolean: FaradayBoolean::ParseBoolean)
